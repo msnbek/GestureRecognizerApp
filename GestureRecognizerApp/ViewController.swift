54df7b1ae421c7749cccad53b5a9d5fcee94b3af
Msnbek <mahmutsenbek@icloud.com>
2022-09-27 13:49:09 +0300

    lastest version

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+   
+    @IBOutlet weak var imageWiew: UIImageView!
+    
+    var isFinch = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageWiew.isUserInteractionEnabled = true
+        
+        let gestureRecongnizer = UITapGestureRecognizer(target: self, action: #selector(changeImage))
+        imageWiew.addGestureRecognizer(gestureRecongnizer)
+       
     }
-
-
+    
+    @objc func changeImage() {
+        print("tapped")
+        
+        if isFinch == true {
+            imageWiew.image = UIImage(named: "reese")
+            labelText.text = "John Reese"
+            isFinch = false
+    }else {
+        imageWiew.image = UIImage(named: "finch")
+        labelText.text = " Harold Finch"
+        isFinch = true
+        
+    }
 }
 
+}
